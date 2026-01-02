c35cc2204fdc9f0fcdbbb1954f8fce8211f306e6
Jose Manuel Montano Mengual <115874492+josemontano1996@users.noreply.github.com>
2026-01-02 14:53:26 +0100

    fix: correct namespace for middleware classes

--- src/Middlewares/SetLocalizedHeaders.php
+++ src/Middlewares/SetLocalizedHeaders.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Josemontano1996\LaravelLocalizationSuite\Http\Middleware;
+namespace Josemontano1996\LaravelLocalizationSuite\Middlewares;
 
 use Closure;
 use Illuminate\Http\Request;
