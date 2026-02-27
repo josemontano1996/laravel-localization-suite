893e222d4a7eef8cfd36a236e2736dfcadfff68a
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2026-02-27 19:33:37 +0100

    chore: installed rector

--- rector.php
+++ rector.php
@@ -3,15 +3,23 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/config',
-        __DIR__.'/src',
-        __DIR__.'/tests',
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withSkip([
+        __DIR__ . '/tests/Integration/runtimes',
+    ])
+
+    // 1. tell Rector what PHP version we support
+    ->withPhpSets(php84: true)                       
+
+    ->withDeadCodeLevel(40)
+    ->withCodeQualityLevel(40)
+    ->withTypeCoverageLevel(40)
+;
